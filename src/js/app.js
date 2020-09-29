dcb0745bc8d652fe9453d3e6f6e5dabb31487d0c
Anton Fedorov <anton.pzkt@gmail.com>
2020-09-30 00:17:55 +0300

    changed scrollFunction. Show and hide the button by opacity property. Changed scroll to top - added scrollIntoView, with smooth scroll(but it doesn't work in IE and Safari)

--- src/js/app.js
+++ src/js/app.js
@@ -2,18 +2,20 @@
 
 const button = document.querySelector('.button');
 console.log('button', button);
+let scroll;
+let scrollSafari;
 
-window.onscroll = function() {scrollFunction()};
+window.addEventListener('scroll', scrollFunction);
 
 function scrollFunction() {
+	console.log('scrollTop', document.documentElement.scrollTop);
 	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		button.style.display = 'flex';
+		button.style.opacity = 1;
 	} else {
-		button.style.display = 'none';
+		button.style.opacity = 0;
 	}
 }
-
 button.addEventListener('click', () => {
-	document.body.scrollTop = 0;
-	document.documentElement.scrollTop = 0;
+	document.body.scrollIntoView({block: 'start', behavior: 'smooth'});
+	document.documentElement.scrollIntoView({block: 'start', behavior: 'smooth'});
 });
